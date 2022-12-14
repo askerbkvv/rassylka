ba6c897f01491a66a7a58b73b74320088b305bd6
meder askerbekov <pythonexample98@gmail.com>
2022-12-14 15:26:02 +0600

    commit

--- fabZ/celery.py
+++ fabZ/celery.py
@@ -14,7 +14,7 @@ app = Celery(
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.conf.broker_url = 'redis://redis:6379/0'
+app.conf.broker_url = 'redis://127.0.0.1:6379/0'
 
 app.conf.timezone = 'UTC'
 
